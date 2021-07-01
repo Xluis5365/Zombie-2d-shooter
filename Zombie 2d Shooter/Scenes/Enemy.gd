bb80029780bfc7023d6ef4b6d2fbff591410892b
Luis melendez <Xluisprox5365@gmail.com>
2021-07-01 15:16:58 -0400

    FInish basic ai work

--- Zombie 2d Shooter/Scenes/Enemy.gd
+++ Zombie 2d Shooter/Scenes/Enemy.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+onready var timer = $Timer
+
 enum {
 	IDLE,
 	ATTACK,
@@ -14,21 +16,14 @@ func _process(delta):
 			print("idle")
 		ATTACK:
 			print("attack")
-		OUTOFSIGHT:
-			pass
 	
 
-func _on_Area2D_body_entered(body):
+func _on_attack_body_entered(body):
 	if body.is_in_group("Player"):
 		state = ATTACK
 
 
 
-
-func match(state):
-	pass
-
-
-func _on_Area2D_body_exited(body):
+func _on_Sight_body_exited(body):
 	if body.is_in_group("Player"):
 		state = IDLE
