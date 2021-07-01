35a56df371f9c739a6f67eb9e1839bfdddc2e505
Luis melendez <Xluisprox5365@gmail.com>
2021-07-01 14:18:37 -0400

    Worked on ai

--- Zombie 2d Shooter/Scenes/Enemy.gd
+++ Zombie 2d Shooter/Scenes/Enemy.gd
@@ -11,16 +11,24 @@ var state = IDLE
 func _process(delta):
 	match state:
 		IDLE:
-			pass
+			print("idle")
 		ATTACK:
-			pass
+			print("attack")
 		OUTOFSIGHT:
 			pass
 	
 
 func _on_Area2D_body_entered(body):
-	if body.is_in_group():
-		pass
+	if body.is_in_group("Player"):
+		state = ATTACK
+
+
+
 
 func match(state):
 	pass
+
+
+func _on_Area2D_body_exited(body):
+	if body.is_in_group("Player"):
+		state = IDLE
