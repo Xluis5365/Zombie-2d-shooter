9ceb97cd24fff0e99195099ea4d90eb55cbe9ec4
Luis melendez <Xluisprox5365@gmail.com>
2021-09-19 15:06:13 -0400

    i added a light texture and added a light to the  scene

--- Zombie 2d Shooter/Scenes/World/MainWorld.gd
+++ Zombie 2d Shooter/Scenes/World/MainWorld.gd
@@ -25,9 +25,9 @@ func _ready() -> void:
 	$Timer.start()
 
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
-		get_tree().notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)
+#func _input(event: InputEvent) -> void:
+#	if event.is_action_pressed("ui_cancel"):
+#		get_tree().notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)
 
 
 func handle_bullet_spawned(bullet: KinematicBody2D, pos: Vector2, direction: Vector2):
